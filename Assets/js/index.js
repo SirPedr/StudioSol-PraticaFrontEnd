610904fa59742c2209845f8778cf143dc8226955
Pedro Paulo <ppagbpedro@gmail.com>
2019-11-08 20:41:04 -0300

    Instanciação da classe comunicadora com o DOM
    Seu objeto foi adicionado à lista de observadores da classe responsável por controlar o fluxo de jogo.

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,5 +1,7 @@
 const ticTacToeGrid = new TicTacToeGrid(3),
-      firstPlayer = new Player("Jogador 1", "X"),
+      gameStateController = new GameStatusController(),
       firstPlayer = new Player("Jogador 1", "X", "#FF3266"),
       secondPlayer = new Player("Jogador 2", "O", "#30CB76"),
       ticTacToeGame = new TicTacToeGame(ticTacToeGrid, firstPlayer, secondPlayer);
+
+ticTacToeGame.gameStateObservers.push(gameStateController.updateElementWithGameState);
