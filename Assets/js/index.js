9bce24c490de2aef763be313ca4085c018c80fb4
Pedro Paulo <ppagbpedro@gmail.com>
2019-11-08 19:47:18 -0300

    Criando script principal para carregar componentes do jogo

---
+++ Assets/js/index.js
@@ -0,0 +1,4 @@
+const ticTacToeGrid = new TicTacToeGrid(3),
+      firstPlayer = new Player("Jogador 1", "X"),
+      secondPlayer = new Player("Jogador 2", "O"),
+      ticTacToeGame = new TicTacToeGame(ticTacToeGrid, firstPlayer, secondPlayer);
