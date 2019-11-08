14f38ac6f7162b26ba5d65981057d2800d3f3bbf
Pedro Paulo <ppagbpedro@gmail.com>
2019-11-08 20:19:36 -0300

    Mudando nome do método de alternar turnos

--- Assets/js/TicTacToeGame.js
+++ Assets/js/TicTacToeGame.js
@@ -22,11 +22,11 @@ class TicTacToeGame {
     handleCellClick(clickedCell) {
         const wasCellMarked = clickedCell.mark(this.currentPlayer.symbol);
         if(wasCellMarked) {
-            this.alternateTurn();
+            this.switchCurrentPlayer();
         }
     }
 
-    alternateTurn() {
+    switchCurrentPlayer() {
         if(this.currentPlayer.symbol === this.firstPlayer.symbol) {
             this.currentPlayer = this.secondPlayer;
         } else {
