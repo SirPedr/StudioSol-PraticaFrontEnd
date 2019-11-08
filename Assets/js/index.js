0727bb19f0e8ed43068423fd8e816afad7a4cb8c
Pedro Paulo <ppagbpedro@gmail.com>
2019-11-08 20:40:20 -0300

    Atribuição de cores a cada um dos jogadores

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,4 +1,5 @@
 const ticTacToeGrid = new TicTacToeGrid(3),
       firstPlayer = new Player("Jogador 1", "X"),
-      secondPlayer = new Player("Jogador 2", "O"),
+      firstPlayer = new Player("Jogador 1", "X", "#FF3266"),
+      secondPlayer = new Player("Jogador 2", "O", "#30CB76"),
       ticTacToeGame = new TicTacToeGame(ticTacToeGrid, firstPlayer, secondPlayer);
