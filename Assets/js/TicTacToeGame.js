e1b3dc7063e8d8900aaae7b2827650e7570aac69
Pedro Paulo <ppagbpedro@gmail.com>
2019-11-09 00:17:49 -0300

    Criação de método para checar as condições de vitória
    Apesar de grande, é simples. Usando os scripts recentemente commitados, o código checa as linhas e colunas procurando uma sequência de símbolos válidos.

--- Assets/js/TicTacToeGame.js
+++ Assets/js/TicTacToeGame.js
@@ -3,7 +3,9 @@ class TicTacToeGame {
         this.gameTable = gameGrid;
         this.firstPlayer = firstPlayer;
         this.secondPlayer = secondPlayer;
+
         this.currentPlayer = firstPlayer;
+        this.playerWhoMadeLastMove = secondPlayer;
 
         this.gameStateObservers = [];
 
@@ -29,25 +31,83 @@ class TicTacToeGame {
 
         if (wasCellMarked) {
             this.switchCurrentPlayer();
-            this.updateGameState(true, false, this.currentPlayer);
+
+            const gameCurrentState = this.hasWinner();
+
+            if (gameCurrentState.hasWinner) {
+                console.log(`VENCEDOR: ${gameCurrentState.currentPlayer.name}`);
+            }
         }
     }
 
     switchCurrentPlayer() {
         if (this.currentPlayer.symbol === this.firstPlayer.symbol) {
             this.currentPlayer = this.secondPlayer;
+            this.playerWhoMadeLastMove = this.firstPlayer;
         } else {
             this.currentPlayer = this.firstPlayer;
+            this.playerWhoMadeLastMove = this.secondPlayer;
+        }
+    }
+
+    hasWinner() {
+        const gridSize = this.gameTable.gridSize;
+
+        let winnerCells = [],
+            tableHasElementsInSequence = false;
+
+        for (let rowNum = 0; rowNum < gridSize; rowNum++) {
+            const currentRow = this.gameTable.getGridRow(rowNum),
+                rowCellValues = GameGridCell.getValidCellsValues(currentRow);
+
+            if (rowCellValues.length === gridSize && ArrayUtil.areElementsIdenthical(rowCellValues)) {
+                tableHasElementsInSequence = true;
+                winnerCells.push(...currentRow);
+                break;
+            }
         }
 
+        for (let colNum = 0; colNum < gridSize; colNum++) {
+            const currentCol = this.gameTable.getGridCol(colNum),
+                colCellValues = GameGridCell.getValidCellsValues(currentCol);
+
+            if (colCellValues.length === gridSize && ArrayUtil.areElementsIdenthical(colCellValues)) {
+                tableHasElementsInSequence = true;
+                winnerCells.push(...currentCol);
+                break;
+            }
+        }
+
+        const mainDiagonalCells = this.gameTable.getGridMainDiagonal(),
+            diagonalCellsValues = GameGridCell.getValidCellsValues(mainDiagonalCells);
+
+        if (diagonalCellsValues.length === gridSize && ArrayUtil.areElementsIdenthical(diagonalCellsValues)) {
+            winnerCells.push(...mainDiagonalCells);
+            tableHasElementsInSequence = true;
+        }
+
+        const secondaryDiagonalCells = this.gameTable.getGridSecondaryDiagonal(),
+            secondaryDiagCellsValues = GameGridCell.getValidCellsValues(secondaryDiagonalCells);
+
+        if (secondaryDiagCellsValues.length === gridSize && ArrayUtil.areElementsIdenthical(secondaryDiagCellsValues)) {
+            winnerCells.push(...secondaryDiagonalCells);
+            tableHasElementsInSequence = true;
+        }
+
+        if (tableHasElementsInSequence) {
+            return this.updateGameState(false, true, this.playerWhoMadeLastMove, winnerCells);
+        } else {
+            return this.updateGameState(true, false, this.currentPlayer);
+        }
 
     }
 
-    updateGameState(isGameRunning, gameHasWinner, currentPlayer) {
+    updateGameState(isGameRunning, gameHasWinner, currentPlayer, winnerCells = null) {
         this.currentGameState = {
             isRunning: isGameRunning,
             hasWinner: gameHasWinner,
-            currentPlayer: currentPlayer
+            currentPlayer: currentPlayer,
+            winnerCells: winnerCells
         }
 
         this.gameStateObservers
