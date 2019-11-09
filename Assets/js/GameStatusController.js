7914d6b40002e6b86461ea6dca8a038b9e0e5e91
Pedro Paulo <ppagbpedro@gmail.com>
2019-11-09 01:34:50 -0300

    Implementação total da classe que informa estado do jogo
    Agora a classe que se comunica com o DOM e exibe ao jogador o estado atual do jogo exibe tanto o estado de vitória (anunciando o jogador vencedor), como empate, também.

--- Assets/js/GameStatusController.js
+++ Assets/js/GameStatusController.js
@@ -1,21 +1,34 @@
 class GameStatusController {
     constructor() {
-        
+
     }
 
     updateElementWithGameState(currentGameState) {
-        const gameStateDOMRepresentation = document.getElementById('gameStateDisplay');
+        const gameStateDOMRepresentation = document.getElementById('gameStateDisplay'),
+            playerDisplayName = document.createElement('strong'),
+            currentPlayer = currentGameState.currentPlayer;
 
-        if(currentGameState.isRunning) {
-            const currentPlayer = currentGameState.currentPlayer,
-                  playerDisplayName = document.createElement('strong');
-            
+
+        gameStateDOMRepresentation.innerHTML = '';
+
+        if (currentGameState.isRunning) {
             playerDisplayName.innerHTML = currentPlayer.name;
 
             gameStateDOMRepresentation.innerHTML = "Vez de: "
-            gameStateDOMRepresentation.style.backgroundColor = currentPlayer.referenceColor
+            gameStateDOMRepresentation.style.backgroundColor = currentPlayer.referenceColor;
 
             gameStateDOMRepresentation.appendChild(playerDisplayName);
+
+        } else if (!currentGameState.hasWinner) {
+            gameStateDOMRepresentation.innerHTML = 'Empate!'
+            gameStateDOMRepresentation.style.backgroundColor = '#FFCD07';
+
+        } else {
+            playerDisplayName.innerHTML = currentGameState.currentPlayer.name;
+
+            gameStateDOMRepresentation.appendChild(playerDisplayName);
+            gameStateDOMRepresentation.innerHTML += ' venceu!';
+            gameStateDOMRepresentation.style.backgroundColor = currentPlayer.referenceColor;
         }
     }
 }
